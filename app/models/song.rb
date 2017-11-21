2a99ee85ea25a6b0fa295d5c8aae2890f97e30f0
Sabrina Jones <sabrina.jones87@gmail.com>
2017-11-21 19:50:10 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -8,4 +8,12 @@ class Song < ActiveRecord::Base
   def artist_name=(name)
     self.artist = Artist.find_or_create_by(name: name)
   end
+
+  def display_artist(song)
+   if song.artist == nil
+     link_to "Add Artist", edit_song_path(song)
+   else
+      link_to song.artist_name, artist_path(song.artist)
+   end
+ end
 end
